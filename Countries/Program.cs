92f73d580927b2604a7d8031da9478baddb37946
Inal0 <138590266+Inal0@users.noreply.github.com>
2023-08-16 13:32:18 +0400

    Add files via upload

---
+++ Countries/Program.cs
@@ -0,0 +1,42 @@
+﻿using Countries;
+using System;
+using System.Collections.Generic;
+
+namespace Countries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Country azerbaycan = new Country
+
+            //{
+            //    Name = "azerbaycan",
+            //    City = new List<City>
+            //    {
+            //        new City { Name = "Baki", Population = 5000000 },
+            //        new City { Name = "Gence", Population = 1000000 },
+            //        new City { Name = "Baku", Population = 2200000 },
+            //        new City { Name = "Samaxi", Population = 330000 },
+            //        new City { Name = "Culfa", Population = 1100000 },
+            //    }
+
+            //};
+            //Country Fransa = new Country();
+            //City Baku = new City();
+            //Fransa.City.Add(Baku);
+
+            City city = new City("a", 1);
+
+
+            City city1 = new City
+            {
+                Name = "a",
+                Population = 1
+        };
+            
+
+        }
+    }  
+}
---
+++ Countries/country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countries
+{
+    class Country
+    {
+        internal string Name;
+
+        public List<City> City { get; internal set; }
+    }
+}
